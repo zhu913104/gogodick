a9afcd158b9e0072a07d5e06f37bd59ef5eb428c
toadwei <jason5363870@gmail.com>
2019-03-06 17:34:34 +0800

    2222

--- test.py
+++ test.py
@@ -22,18 +22,17 @@ while True:
     
     frame = grabscreen.getWindow_Img(hwnd)
     frame = cv2.cvtColor(frame,cv2.COLOR_RGB2GRAY)
-    frame1  =  efff(frame,0.7)
-    print(frame1.dtype)
-    print(frame.dtype)
-    hist = cv2.calcHist([frame1], [0], None, [256], [0, 256])
+    frame  =  efff(frame,0.6)
+    frame = frame[28:,:1600]
+    hist = cv2.calcHist([frame], [0], None, [256], [0, 256])
     
     # fig = plt.figure()
     plt.ion()
     plt.plot(hist)
     plt.show()
-    
+    print(frame.shape)
     plt.pause(0.1)
-    cv2.imshow("screen box", frame1)
+    cv2.imshow("screen box", frame)
     plt.cla()
     k = cv2.waitKey(30)&0xFF #64bits! need a mask
     if k ==27:
