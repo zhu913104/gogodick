16cd4f50ad763da9e1ac4da51695150c4c90d165
toadwei <jason5363870@gmail.com>
2019-03-15 02:20:01 +0800

    80%

--- p2u.py
+++ p2u.py
@@ -9,16 +9,49 @@ import pyautogui
 
 hwnd = grabscreen.FindWindow_bySearch("envs")
 
+log = np.array([0,0,0])
+
 reword = 0
+t = time.time()
+act = "forword"
+for i in range(4,0,-1):
+    print(i)
+    time.sleep(1)
 
 def normalization_fram(x,m):    
     y = np.array((x**m)/(255**m)*255,dtype=np.uint8)
     return y
 
+def forword():
+    pyautogui.keyUp('d')
+    pyautogui.keyUp('a')
+    pyautogui.keyDown('w')
+    return "forword"
 
-red = (0,0,255)
 
-one = True
+def left():
+    pyautogui.keyUp('d')
+    pyautogui.keyDown('a')
+    return "left" 
+
+def right():
+    pyautogui.keyUp('a')
+    pyautogui.keyDown('d')
+    return "right"
+  
+
+def _stop():
+    pyautogui.keyUp('d')
+    pyautogui.keyUp('a')
+    pyautogui.keyUp('w')
+    time.sleep(1)
+    pyautogui.keyDown('w')
+    
+
+
+
+act =forword()
+
 
 while True:
     #  Wait for next request from client
@@ -27,22 +60,34 @@ while True:
     frame = cv2.cvtColor(frame,cv2.COLOR_RGB2GRAY)
     frameNL  =  normalization_fram(frame,0.6)
 
+    if int(time.time()-t ) %4==0 and act!="forword":
+        act =  forword()
+    elif int(time.time()-t ) %4==1 and act!="left":
+        t1 = time.time()
+        act =left()
+    elif int(time.time()-t ) %4==2 and act!="forword":
+        act =forword()
+    elif int(time.time()-t ) %4==3 and act!="right":
+        t1 = time.time()
+        act =right()
+
+
+
     rewordpix = frame[7,-7]
+    
     if rewordpix == 255:
         reword = 1
-        cv2.imwrite("p.png",frame)
-        cv2.imwrite("pNL.png",frameNL)
     elif rewordpix ==0:
         reword = -1
-        cv2.imwrite("n.png",frame)
-        cv2.imwrite("nNL.png",frameNL)
-        one = False
+        _stop()
     else:
         reword = 0
-        cv2.imwrite("f.png",frame)
-        cv2.imwrite("fNL.png",frameNL)
-        
-    print(reword)
+    
+    log = np.vstack((log,[act,reword,time.time()-t]))
+
+
+    print([act,reword,time.time()-t])
+
     
 
     cv2.imshow("screen box", frame)
